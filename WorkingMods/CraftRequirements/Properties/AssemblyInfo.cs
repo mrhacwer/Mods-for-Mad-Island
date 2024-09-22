6f3133b0d199b17180320c8f39b715c8a2b1cedd
Hacwer <mrhacwer@hotmail.com.br>
2024-09-22 01:07:06 -0300

    Fixing mistakes on AssemblyInfo.cs Wrong credits.

--- WorkingMods/CraftRequirements/Properties/AssemblyInfo.cs
+++ WorkingMods/CraftRequirements/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using MelonLoader;
 using CraftRequirements;
 
-[assembly: MelonInfo(typeof(CraftRequirementsMOD), "Craft Requirements", "0.1.0", "MRHacwer")] //Info of the mod.
+[assembly: MelonInfo(typeof(CraftRequirementsMOD), "Craft Requirements", "0.1.0", "MRHacwer a port of Yotan Mods for MelonLoader")] //Info of the mod.
 [assembly: MelonGame("Emade Plus", "Mad Island")] //Mod for what game.
 [assembly: AssemblyTitle("This mod highlights in red the items that are missing for crafting on all workbenches.")] //Mod Description.
 [assembly: AssemblyCopyright("BSD-3-Clause")] //Copyright Assembly.
