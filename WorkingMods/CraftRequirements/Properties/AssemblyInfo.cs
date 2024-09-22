1ccf30a37b0c6c3fd2b8863347f65509a8dff4f9
Hacwer <mrhacwer@hotmail.com.br>
2024-09-22 00:52:33 -0300

    Changed the description of the new mod.

--- WorkingMods/CraftRequirements/Properties/AssemblyInfo.cs
+++ WorkingMods/CraftRequirements/Properties/AssemblyInfo.cs
@@ -4,5 +4,5 @@ using CraftRequirements;
 
 [assembly: MelonInfo(typeof(CraftRequirementsMOD), "Craft Requirements", "0.1.0", "MRHacwer")] //Info of the mod.
 [assembly: MelonGame("Emade Plus", "Mad Island")] //Mod for what game.
-[assembly: AssemblyTitle("Show in red the items that are missing and that you need to create this item on any workstation.")] //Mod Description.
+[assembly: AssemblyTitle("This mod highlights in red the items that are missing for crafting on all workbenches.")] //Mod Description.
 [assembly: AssemblyCopyright("BSD-3-Clause")] //Copyright Assembly.
